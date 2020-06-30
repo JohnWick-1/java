02e152a0dd44dfe03ebdb56f88fadc4d408f932e
JohnWick-1 <sbkapte1@gmail.com>
2020-06-30 19:23:27 +0530

    js added

--- static/js/function.js
+++ static/js/function.js
@@ -1,19 +1,16 @@
     function average() {
 
-        alert("hi")
+
         var eng = document.getElementById("eg").value;
 
         var math = document.getElementById("mt").value;
 
         var phy = document.getElementById("ph").value;
 
-        int avg = (eng.value + math.value + phy.value);
-        alert(eng)
-        alert(math)
-        alert(phy)
-        alert(avg)
+        var avg = (parseFloat(eng) + parseFloat(math) + parseFloat(phy))/3;
 
-        document.getElementById("ag").value=avg;
 
-        return avg;
+        document.getElementById("ag").value=avg.toFixed(2);
+
     }
+
